be3c06a42503abe2bc520c12859699083adb1fbb
Brahma Putra <brahmaputra1996@gmail.com>
2021-07-03 23:30:17 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<v-container>
+
+  <div class="d-flex justify-center">
+    <div style="max-width:900px;width:90%;padding:30px;background-color:#fff;">  
+      <iframe src="/staticweb/index.html" width="100%" height="480px" style="border:none;">
+      </iframe>
+    </div>
+  </div>
+
+</v-container>
+</template>
+
+<script>
+
+</script>
